3d7b1bcc4dd135ca2035627eaa091fb01bb5b6b6
zabry0 <ondrejzabrodsky@gmail.com>
2024-04-26 12:10:46 +0200

    nepovedlose

---
+++ Untitled-1.py
@@ -0,0 +1,23 @@
+
+print ('Vítejte v obchodě grafických karet')
+print ('Zde je váš výběr')
+
+             #1         #2       #3         #4         #5
+zbozi = ['GTX 1660, GTX 1080, RTX 2060, RTX 3070, RTX 4080']
+kosik = []
+
+vyber = input 
+
+def vypis_zbozi():
+    print ('Zde je váš výběr') 
+
+def vypis_kosik():
+    print ('Zde je váš košík')
+
+def vypis_pole():
+for x in range (len(prvek)):
+    if vyber.lower == 'konec'
+
+
+else: 
+print ('Neplatná volba')
